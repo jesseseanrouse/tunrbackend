5f0776f1c33bacbf2ea292231ed29ef62d438987
Jesse Rouse <jesserouse@apples-iMac-2.local>
2020-11-16 08:16:38 -0800

    set up rails and cors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Song.create([
+    {title: 'test', artist: 'test', time: '1:10', favorite: false},
+    {title: 'test1', artist: 'test1', time: '1:11', favorite: false},
+    {title: 'test2', artist: 'test2', time: '1:12', favorite: false},
+    {title: 'test3', artist: 'test3', time: '1:13', favorite: false}
+])
